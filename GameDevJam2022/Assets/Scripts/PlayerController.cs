12d16aca63d42e44c74e114a7a558eab65ccc916
Valentín Dri <51834435+valentindri@users.noreply.github.com>
2022-04-23 19:19:54 -0300

    Separation of player scripts

--- GameDevJam2022/Assets/Scripts/PlayerController.cs
+++ GameDevJam2022/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,11 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] public float moveSpeed;
     [SerializeField] public float jumpForce;
-    [SerializeField] public float fallThresholdVelocity = 1f;
+    
     private bool isGrounded;
     private float moveHorizontal;
     private float moveVertical;
-    private float maxYvelocity;
+    
 
 
     // Start is called before the first frame update
@@ -36,10 +36,10 @@ public class PlayerController : MonoBehaviour
 
         if (moveHorizontal > 0f || moveHorizontal < 0f)
         {
-            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed,0f), ForceMode2D.Impulse);
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed,0f), ForceMode2D.Force);
         }
 
-        if(isGrounded && moveVertical > 0.1f)
+        if(isGrounded && moveVertical > 0f)
         {
             rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
             isGrounded = false;
@@ -47,65 +47,20 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        bool previousGrounded = isGrounded;
-
         if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Bush")
         {
             isGrounded = true;
         }
-        //Debug.Log(previousGrounded);
-        //Debug.Log(isGrounded);
-        if (!previousGrounded && isGrounded)
-        {
-            float asd = rb2D.velocity.y + fallThresholdVelocity;
-
-            Debug.Log(asd);
-
-            float damage = Mathf.Abs(rb2D.velocity.y);
-            //Debug.Log("damash"+damage);
-            if (rb2D.velocity.y < -fallThresholdVelocity)
-            {
-                GetDamage(damage);
-            }
-        }
-    }
-   
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag != "Bush" && collision.gameObject.tag != "Bouncy")
-        {
-            //Debug.Log("Not a bush "+" Collision speed:"+GetComponent<Rigidbody2D>().velocity.magnitude);
-            //Debug.Log("Not a bush "+" Collision :"+GetComponent<Rigidbody2D>().velocity);
-
-            Vector2 impactVelocity = collision.relativeVelocity;
-            float minimumDamageThreshold = 20f;
-            float collisionDamageScale = 10;
-
-            // Subtracting a minimum threshold can avoid tiny scratches at negligible speeds.
-            float magnitude = Mathf.Max(0f, impactVelocity.magnitude - minimumDamageThreshold);
-
-            // Using sqrMagnitude can feel good here,
-            // making light taps less damaging and high-speed strikes devastating.
-
-            float damage = magnitude * collisionDamageScale;
-            //Debug.Log("Impact velocity: " +collision.relativeVelocity);
-            //Debug.Log("Impact magnitude: " +collision.relativeVelocity.magnitude );
-            //Debug.Log("Damage: " + damage);
-            
-        }
-
-
-
-
-
     }
 
-    void GetDamage(float damage)
-    {
-        Debug.Log("Received damage!!"+ damage );
-    }
-    
+
+
+
+
+
+
+
+
 }
