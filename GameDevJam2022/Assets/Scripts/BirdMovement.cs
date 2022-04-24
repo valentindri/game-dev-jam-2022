efc3635b0ed0db649beeae819a15873acbf5d29a
Valentín Dri <51834435+valentindri@users.noreply.github.com>
2022-04-24 18:59:06 -0300

    Fixed bird

--- GameDevJam2022/Assets/Scripts/BirdMovement.cs
+++ GameDevJam2022/Assets/Scripts/BirdMovement.cs
@@ -10,15 +10,23 @@ public class BirdMovement : MonoBehaviour
         
     }
 
-    private bool dirRight = true;
-    public float speed = 5f;
+    
+    private Vector3 pos1 = new Vector3(-14, 5, 0);
+    private Vector3 pos2 = new Vector3(18, 5, 0);
+    public float speed = 1.0f;
 
     void Update()
     {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+        if (transform.position.x >= pos2.x -1)
+        {
+            gameObject.transform.localScale = new Vector2(1.5f, 1.5f);
+        }
+        else if(transform.position.x <= pos1.x + 1)
+        {
+            gameObject.transform.localScale = new Vector2(-1.5f, 1.5f);
+        }
         
-        transform.Translate((dirRight? Vector2.right : Vector2.left) * speed * Time.deltaTime);
-
-        dirRight = transform.position.x >= 15f ? false : true;
-
     }
+    
 }
