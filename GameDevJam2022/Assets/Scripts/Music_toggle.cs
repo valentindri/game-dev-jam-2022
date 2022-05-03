01261caa36698d71a2ee1cf7e2ef45d5ecb2a1ad
Camila Arciniega <54546162+am-camila@users.noreply.github.com>
2022-05-03 16:02:53 -0300

    Added music toggle animation

--- GameDevJam2022/Assets/Scripts/Music_toggle.cs
+++ GameDevJam2022/Assets/Scripts/Music_toggle.cs
@@ -6,15 +6,28 @@ using UnityEngine.UI;
 public class Music_toggle : MonoBehaviour
 {
     private bool isMuted = false;
+    [SerializeField] private Sprite[] speakerSprites;
+    private Image speakerImage;
+    private int speakerState; //0=muted, 1=unmuted
 
     void Start()
     {
         isMuted = false;
+        speakerImage = GetComponent<Button>().image;
+        ToggleSpeakerImage();
     }
 
     public void Mute()
     {
         isMuted = !isMuted;
         AudioListener.pause = isMuted;
+        ToggleSpeakerImage();
+
+    }
+
+    public void ToggleSpeakerImage()
+    {
+        speakerState = isMuted ? 1 : 0;
+        speakerImage.sprite = speakerSprites[speakerState];
     }
 }
